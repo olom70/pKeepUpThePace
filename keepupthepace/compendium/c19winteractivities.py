ae41a1aef33c2a7bb68f7ae3710b78ee5346fc81
olom <olom@noscreen.lan>
2020-03-07 11:16:27 +0100

    compendium : all classes have now their data

--- keepupthepace/compendium/c19winteractivities.py
+++ keepupthepace/compendium/c19winteractivities.py
@@ -6,18 +6,99 @@ import keepupthepace.compendium.abstractcompendium
 class WinterActivities(keepupthepace.compendium.abstractcompendium.Compendium):
 
     def __init__(self):
-        self.metValue  = {
-        
-        }
+        self.metValue  = {19005 : 7.5
+            ,19006 : 2.5
+            ,19010 : 6.0 
+            ,19011 : 2.0 
+            ,19018 : 14.0
+            ,19020 : 5.5 
+            ,19030 : 7.0 
+            ,19040 : 9.0 
+            ,19050 : 13.3
+            ,19060 : 7.0 
+            ,19075 : 7.0 
+            ,19080 : 6.8
+            ,19090 : 9.0
+            ,19100 : 12.5
+            ,19110 : 15.0
+            ,19130 : 15.5
+            ,19135 : 13.3
+            ,19140 : 13.5
+            ,19150 : 4.3
+            ,19160 : 5.3
+            ,19170 : 8.0
+            ,19175 : 12.5
+            ,19180 : 7.0 
+            ,19190 : 5.3
+            ,19192 : 10.0
+            ,19200 : 3.5 
+            ,19202 : 2.0
+            ,19252 : 5.3
+            ,19254 : 7.5
+            ,19260 : 2.5 }
+
+        self.metDescription  = {19005 : "dog sledding, mushing"
+            ,19006 : "dog sledding, passenger"
+            ,19010 : "moving ice house, set up/drill holes"
+            ,19011 : "ice fishing, sitting"
+            ,19018 : "skating, ice dancing"
+            ,19020 : "skating, ice, 9 mph or less"
+            ,19030 : "skating, ice, general (Taylor Code 360)"
+            ,19040 : "skating, ice, rapidly, more than 9 mph, not competitive"
+            ,19050 : "skating, speed, competitive"
+            ,19060 : "ski jumping, climb up carrying skis"
+            ,19075 : "skiing, general"
+            ,19080 : "skiing, cross country, 2.5 mph, slow or light effort, ski walking"
+            ,19090 : "skiing, cross country, 4.0-4.9 mph, moderate speed and effort, general"
+            ,19100 : "skiing, cross country, 5.0-7.9 mph, brisk speed, vigorous effort"
+            ,19110 : "skiing, cross country, >8.0 mph, elite skier, racing"
+            ,19130 : "skiing, cross country, hard snow, uphill, maximum, snow mountaineering"
+            ,19135 : "skiing, cross-country, skating"
+            ,19140 : "skiing, cross-country, biathlon, skating technique"
+            ,19150 : "skiing, downhill, alpine or snowboarding, light effort, active time only"
+            ,19160 : "skiing, downhill, alpine or snowboarding, moderate effort, general, active time only"
+            ,19170 : "skiing, downhill, vigorous effort, racing"
+            ,19175 : "skiing, roller, elite racers"
+            ,19180 : "sledding, tobogganing, bobsledding, luge (Taylor Code 370)"
+            ,19190 : "snow shoeing, moderate effort"
+            ,19192 : "snow shoeing, vigorous effort"
+            ,19200 : "snowmobiling, driving, moderate"
+            ,19202 : "snowmobiling, passenger"
+            ,19252 : "snow shoveling, by hand, moderate effort"
+            ,19254 : "snow shoveling, by hand, vigorous effort"
+            ,19260 : "snow blower, walking and pushing"}
 
 
-        self.metDescription  = {
-
-        }
-
-        self.metDescription_fr  = {
-
-        }        
+        self.metDescription_fr  = {19005 : "randonnée en traîneau à chiens en tant que musher"
+            ,19006 : "randonnée en traîneau à chiens en tant que passager"
+            ,19010 : "retrait de la glace, maison, créer/forer des trous"
+            ,19011 : "pêche sur glace, position assise"
+            ,19018 : "patinage, dance sur glace"
+            ,19020 : "patinage sur glace à 14,5 km/h ou moins"
+            ,19030 : "patinage sur glace, général (code Taylor 360)"
+            ,19040 : "patinage sur glace, rythme rapide, plus de 14,5 km/h, hors compétition"
+            ,19050 : "patinage de vitesse, compétition"
+            ,19060 : "saut à ski, monter en portant des skis"
+            ,19075 : "ski, général"
+            ,19080 : "ski de fond à 4 km/h, rythme lent ou effort léger, marche nordique"
+            ,19090 : "ski de fond à 6,5-8 km/h, rythme et effort modérés, général"
+            ,19100 : "ski de fond à 8-12,5 km/h, rythme rapide, effort vigoureux"
+            ,19110 : "ski de fond à plus de 13 km/h, skieur d’élite, course"
+            ,19130 : "ski de fond, neige dure, montée, maximum, alpinisme"
+            ,19135 : "ski de fond, patinage"
+            ,19140 : "ski de fond, biathlon, pas de patineur"
+            ,19150 : "ski, descente, ski alpin ou snowboard, effort léger, périodes d’activité uniquement"
+            ,19160 : "ski, descente, ski alpin ou snowboard, effort modéré, général, périodes d’activité uniquement"
+            ,19170 : "ski, descente, effort vigoureux, course"
+            ,19175 : "ski, ski à roulettes, coureurs d’élite"
+            ,19180 : "traîneau, toboggan, bobsleigh, luge (code Taylor 370)"
+            ,19190 : "raquettes à neige, effort modéré"
+            ,19192 : "raquettes à neige, effort vigoureux"
+            ,19200 : "moto des neiges, conduire, effort modéré"
+            ,19202 : "moto des neiges, passager"
+            ,19252 : "déblayer la neige à la pelle, à la main, effort modéré"
+            ,19254 : "déblayer la neige à la pelle, à la main, effort vigoureux"
+            ,19260 : "passer la souffleuses à neige, marcher et pousser"}
 
     def printValues(self):
         print("Beginning dump for 'WinterActivities' ")
@@ -29,4 +110,4 @@ class WinterActivities(keepupthepace.compendium.abstractcompendium.Compendium):
 if __name__ == "__main__":
     b = WinterActivities()
     b.printValues()
-    print(b.getMetValue(2205))
+    print(b.getMetValue(19018))
--- keepupthepace/compendium/c20religiousactivities.py
+++ keepupthepace/compendium/c20religiousactivities.py
@@ -6,18 +6,83 @@ import keepupthepace.compendium.abstractcompendium
 class ReligiousActivities(keepupthepace.compendium.abstractcompendium.Compendium):
 
     def __init__(self):
-        self.metValue  = {
-        
-        }
+        self.metValue  = {20000 : 1.3
+            ,20001 : 2.0
+            ,20005 : 1.8
+            ,20010 : 1.3 
+            ,20015 : 1.3 
+            ,20020 : 2.0 
+            ,20025 : 1.3 
+            ,20030 : 1.8 
+            ,20035 : 2.0 
+            ,20036 : 2.0 
+            ,20037 : 3.5
+            ,20038 : 4.3
+            ,20039 : 2.0 
+            ,20040 : 5.0 
+            ,20045 : 2.5 
+            ,20046 : 2.0 
+            ,20047 : 3.3 
+            ,20050 : 1.5 
+            ,20055 : 2.0 
+            ,20060 : 3.3
+            ,20061 : 4.0 
+            ,20065 : 3.5 
+            ,20095 : 4.5
+            ,20100 : 1.3 }
 
 
-        self.metDescription  = {
+        self.metDescription  = {20000 : "sitting in church, in service, attending a ceremony, sitting quietly"
+            ,20001 : "sitting, playing an instrument at church"
+            ,20005 : "sitting in church, talking or singing, attending a ceremony, sitting, active participation"
+            ,20010 : "sitting, reading religious materials at home"
+            ,20015 : "standing quietly in church, attending a ceremony"
+            ,20020 : "standing, singing in church, attending a ceremony, standing, active participation"
+            ,20025 : "kneeling in church or at home, praying"
+            ,20030 : "standing, talking in church"
+            ,20035 : "walking in church"
+            ,20036 : "walking, less than 2.0 mph, very slow"
+            ,20037 : "walking, 3.0 mph, moderate speed, not carrying anything"
+            ,20038 : "walking, 3.5 mph, brisk speed, not carrying anything"
+            ,20039 : "walk/stand combination for religious purposes, usher"
+            ,20040 : "praise with dance or run, spiritual dancing in church"
+            ,20045 : "serving food at church"
+            ,20046 : "preparing food at church"
+            ,20047 : "washing dishes, cleaning kitchen at church"
+            ,20050 : "eating at church"
+            ,20055 : "eating/talking at church or standing eating, American Indian Feast days"
+            ,20060 : "cleaning church"
+            ,20061 : "general yard work at church"
+            ,20065 : "standing, moderate effort (e.g., lifting heavy objects, assembling at fast rate)"
+            ,20095 : "standing, moderate-to-heavy effort, manual labor, lifting ≥ 50 lbs, heavy maintenance"
+            ,20100 : "typing, electric, manual, or computer"}
 
-        }
 
-        self.metDescription_fr  = {
+        self.metDescription_fr  = {20000 : "être assis à l'église, pendant la messe, assister à une cérémonie, être assis en silence"
+            ,20001 : "être assis, jouer d'un instrument à l'église"
+            ,20005 : "être assis à l'église, parler ou chanter, assister à une cérémonie, être assis, participation active"
+            ,20010 : "être assis, lire des textes religieux à la maison"
+            ,20015 : "être debout à l'église (en silence), assister à une cérémonie"
+            ,20020 : "être debout, chanter à l'église, assister à une cérémonie, être debout, participation active"
+            ,20025 : "être à genoux à l'église ou à la maison, prier"
+            ,20030 : "être debout, parler à l'église"
+            ,20035 : "marcher dans l'église"
+            ,20036 : "marcher à moins de 3 km/h, rythme très lent"
+            ,20037 : "marcher à 5 km/h, rythme modéré, sans rien porter"
+            ,20038 : "marcher à 5,5 km/h, rythme rapide, sans rien porter"
+            ,20039 : "combinaison marcher/se tenir debout à des fins religieuses, placer l'assistance"
+            ,20040 : "faire une louange en dansant ou en courant, danse spirituelle dans l'église"
+            ,20045 : "servir des aliments à l'église"
+            ,20046 : "préparer des aliments à l'église"
+            ,20047 : "faire la vaisselle, nettoyer la cuisine à l'église"
+            ,20050 : "manger à l'église"
+            ,20055 : "manger/parler à l'église ou manger debout, fêtes religieuses"
+            ,20060 : "nettoyer l'église"
+            ,20061 : "travail général dans la cour de l'église"
+            ,20065 : "être debout, effort modéré (soulever des objets lourds, assembler à un rythme rapide)"
+            ,20095 : "être debout, effort modéré à important, travail manuel, soulevez des charges ≥ 22,5 kg, maintenance lourde"
+            ,20100 : "taper des documents avec une machine à écrire électrique, mécanique ou un ordinateur"}
 
-        }        
 
     def printValues(self):
         print("Beginning dump for 'ReligiousActivities' ")
@@ -29,4 +94,4 @@ class ReligiousActivities(keepupthepace.compendium.abstractcompendium.Compendium
 if __name__ == "__main__":
     b = ReligiousActivities()
     b.printValues()
-    print(b.getMetValue(2205))
+    print(b.getMetValue(20095))
