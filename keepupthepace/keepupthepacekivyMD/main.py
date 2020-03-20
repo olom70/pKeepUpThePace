259cddb9ed71b454d85c6a67a188456979ae51d0
olom <olom@noscreen.lan>
2020-03-21 00:21:30 +0100

    first layout that works with kivyMD

--- keepupthepace/keepupthepacekivyMD/main.py
+++ keepupthepace/keepupthepacekivyMD/main.py
@@ -49,7 +49,104 @@ class ProfileUIX(MDBottomNavigationItem):
     pass
 
 class Metrics(MDBottomNavigationItem):
-    pass
+    '''
+        Screen to enter all the metrics related to a profile
+    '''
+    def initProfileUpdate(self, metricToSave, *args):
+        '''
+            Update (in memory only) the current Profile with the modified value
+        '''
+        if (metricToSave == 'heightIntegerPart'):
+            valueToSave = int(self.ids.profile_height_integerpart.text)
+            if (valueToSave >=0 and valueToSave <= 2):
+                kivy.app.App.get_running_app().myProfile.heightIntegerPart = valueToSave
+
+        if (metricToSave == 'profileName'):
+            valueToSave = str(self.ids.profile_name.text)
+            if (len(valueToSave) > 0):
+                kivy.app.App.get_running_app().myProfile.profileName = valueToSave
+                 
+        if (metricToSave == 'heightDecimalPart'):
+            valueToSave = int(self.ids.profile_height_decimalpart.text)
+            if (valueToSave >= 0 and valueToSave <= 99):
+                kivy.app.App.get_running_app().myProfile.heightDecimalPart = valueToSave
+
+        if (metricToSave == 'weightIntegerPart'):
+            valueToSave = int(self.ids.profile_weight_integerpart.text)
+            if (valueToSave >= 1):
+                kivy.app.App.get_running_app().myProfile.weightIntegerPart = valueToSave
+
+        if (metricToSave == 'weightDecimalPart'):
+            valueToSave = int(self.ids.profile_weight_decimalpart.text)
+            if (valueToSave >= 0 and valueToSave <= 99):
+                kivy.app.App.get_running_app().myProfile.weightDecimalPart = valueToSave
+
+        if (metricToSave == 'age'):
+            valueToSave = int(self.ids.profile_age.text)
+            if (valueToSave >= 1):
+                kivy.app.App.get_running_app().myProfile.age = valueToSave
+
+        if (metricToSave == 'gender'):
+            valueToSave = str(self.ids.profile_gender.text)
+            if (valueToSave == str(kivy.app.App.get_running_app().myProfile.getAllGenders()['0'])):
+                 kivy.app.App.get_running_app().myProfile.gender = enumandconst.Gender.FEMALE
+            if (valueToSave == str(kivy.app.App.get_running_app().myProfile.getAllGenders()['1'])):
+                 kivy.app.App.get_running_app().myProfile.gender = enumandconst.Gender.MALE
+
+        if (metricToSave == 'activityFactor'):
+            valueToSave = str(self.ids.profile_activityFactor.text)
+            if (valueToSave == str(kivy.app.App.get_running_app().myProfile.getAllActivityFactors()['1'])):
+                 kivy.app.App.get_running_app().myProfile.activityFactor = enumandconst.ActivityFactor.SEDENTARY
+            if (valueToSave == str(kivy.app.App.get_running_app().myProfile.getAllActivityFactors()['2'])):
+                 kivy.app.App.get_running_app().myProfile.activityFactor = enumandconst.ActivityFactor.LIGHTLYACTIVE
+            if (valueToSave == str(kivy.app.App.get_running_app().myProfile.getAllActivityFactors()['3'])):
+                 kivy.app.App.get_running_app().myProfile.activityFactor = enumandconst.ActivityFactor.MODERATELYACTIVE
+            if (valueToSave == str(kivy.app.App.get_running_app().myProfile.getAllActivityFactors()['4'])):
+                 kivy.app.App.get_running_app().myProfile.activityFactor = enumandconst.ActivityFactor.ACTIVE
+            if (valueToSave == str(kivy.app.App.get_running_app().myProfile.getAllActivityFactors()['5'])):
+                 kivy.app.App.get_running_app().myProfile.activityFactor = enumandconst.ActivityFactor.VIGOROUS
+            if (valueToSave == str(kivy.app.App.get_running_app().myProfile.getAllActivityFactors()['6'])):
+                 kivy.app.App.get_running_app().myProfile.activityFactor = enumandconst.ActivityFactor.VIGOROUSLYACTIVE
+
+        if (metricToSave == 'femaletriceps'):
+            valueToSave = int(self.ids.profile_female_triceps.text)
+            if (valueToSave >= 1):
+                kivy.app.App.get_running_app().myProfile.triceps = valueToSave
+
+        if (metricToSave == 'femalesuprailium'):
+            valueToSave = int(self.ids.profile_female_suprailium.text)
+            if (valueToSave >= 1):
+                kivy.app.App.get_running_app().myProfile.suprailium = valueToSave
+
+        if (metricToSave == 'femalethigh'):
+            valueToSave = int(self.ids.profile_female_thigh.text)
+            if (valueToSave >= 1):
+                kivy.app.App.get_running_app().myProfile.thigh = valueToSave
+
+        if (metricToSave == 'maletriceps'):
+            valueToSave = int(self.ids.profile_male_triceps.text)
+            if (valueToSave >= 1):
+                kivy.app.App.get_running_app().myProfile.triceps = valueToSave
+
+        if (metricToSave == 'maleabdomen'):
+            valueToSave = int(self.ids.profile_male_abdomen.text)
+            if (valueToSave >= 1):
+                kivy.app.App.get_running_app().myProfile.abdomen = valueToSave
+
+        if (metricToSave == 'malethigh'):
+            valueToSave = int(self.ids.profile_male_thigh.text)
+            if (valueToSave >= 1):
+                kivy.app.App.get_running_app().myProfile.thigh = valueToSave
+
+    def on_focus(self, focused, metricToSave):
+        '''
+            Triggered by TextInput.
+            Is intended to save the value when the focus is lost
+        '''
+        if focused:
+            pass
+        else:
+            self.initProfileUpdate(metricToSave)
 
 class Compendium(MDBottomNavigationItem):
     pass
@@ -149,6 +246,22 @@ class KeepUpThePaceMDApp(MDApp):
             tr._('19-Winter Activities'),
             tr._('20-Religious Activities'),
             tr._('21-Volunteer Activities')]
+
+    def doThingsBetweenScreen(self):
+        '''
+            save profiles when leaving some selectted tabs
+        '''
+        self.myProfile.computeAll()
+        persistence.saveprofiles()
+        self.kbbmi = self.myProfile.displaybBMI()
+        self.knbmi = self.myProfile.displaynBMI()
+        self.krmr1918, self.krmr1984, self.krmr1990 = self.myProfile.displayRMR()
+        self.krmrml1918, self.krmrml1984, self.krmrml1990 = self.myProfile.displayRMRml()
+        self.khbe1918, self.khbe1984, self.khbe1990 = self.myProfile.displayHBE()
+        self.kqfp, self.kefp = self.myProfile.displayFAT()
+
+    def on_stop(self):
+        persistence.saveprofiles()
 
     def build(self):
         pass
